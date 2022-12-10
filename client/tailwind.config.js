b3ec0f53726a2ddc971f6c0723bcb9dcd6deb301
Sofia Ling <sofialing@live.se>
2022-12-10 13:25:58 +0100

    Update config and settings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['canada-type-gibson', 'sans-serif'],
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['canada-type-gibson', 'sans-serif'],
+            },
+        },
     },
-  },
-  corePlugins: {
-    aspectRatio: false,
-  },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+    corePlugins: {
+        aspectRatio: false,
+    },
+    plugins: [require('@tailwindcss/aspect-ratio')],
+};
